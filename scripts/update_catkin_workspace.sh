91cd36affcdf713999b0fd40b62f883f38497e3c
Jenkins <jenkins@ubuntu.(none)>
2014-04-16 00:27:37 -0700

    replaced rosws to wstool

--- scripts/update_catkin_workspace.sh
+++ scripts/update_catkin_workspace.sh
@@ -2,26 +2,28 @@
 # merge new entries in workspace (.rosinstall) file and update all tracked repos
 
 # variables
-SETUP_FILE=$WORKSPACE"/setup.bash"
+ROS_SETUP_FILE="/opt/ros/hydro/setup.bash"
+SETUP_FILE=$WORKSPACE"/devel/setup.bash"
 ROS_INSTALL_FILE=${JENKINS_HOME}/jenkins_config/workspaces/${JOB_NAME}.rosinstall
 
 # merging workspace file
 if [ -f $ROS_INSTALL_FILE ];
 then
 	# reset ros workspace file
-	echo "" > "$WORKSPACE/.rosinstall"
+	echo "" > "$WORKSPACE/src/.rosinstall"
 
 	echo "updating workspace file $ROS_INSTALL_FILE"
-	cd ${WORKSPACE}
-	rosws merge ${ROS_INSTALL_FILE} -r -y
+	cd ${WORKSPACE}/src
+    wstool merge ${ROS_INSTALL_FILE} -r -y
 else
 	echo "workspace .rosinstall file not found, skip update"
 fi
 
 #sourcing setup file
+source $ROS_SETUP_FILE
 source $SETUP_FILE
 
 #updating repositories
 echo "updating repositories"
-rosws update --delete-changed-uris -v
+wstool update --delete-changed-uris -v -j10
 
