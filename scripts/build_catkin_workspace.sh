ff332bd39fe006b6ba389341d1157be5f5de1a29
Jenkins <jenkins@sectoid.(none)>
2014-04-20 07:53:14 -0700

    Added catkin_make_ignore

--- scripts/build_catkin_workspace.sh
+++ scripts/build_catkin_workspace.sh
@@ -2,11 +2,26 @@
 
 # variables
 ROS_SETUP_FILE="/opt/ros/hydro/setup.bash"
+IGNORE_PATH=${JENKINS_HOME}/jenkins_config/catkin_make_ignore
 
 echo
 echo "============================================================================"
 echo "Building catkin workspace"
 echo "============================================================================"
 
+# Read the ignore file
+ignorestr=""
+while read p; do
+  echo "Ignoring $p"
+
+  if [ "$grepstr" == "" ]
+  then
+      ignorestr="$p"
+  else
+      ignorestr="${ignorestr};$p"
+  fi
+done < $IGNORE_PATH
+echo
+
 source $ROS_SETUP_FILE
-catkin_make
+catkin_make -DCATKIN_BLACKLIST_PACKAGES="${ignorestr}"
