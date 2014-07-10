8dee05049b6e0268193b4d47629b26c2670391b2
Jenkins <jenkins@sectoid.(none)>
2014-07-10 15:25:42 -0700

    added "Do not leave a blank line after public/private/protected" filter.

--- scripts/run_cpplint.bash
+++ scripts/run_cpplint.bash
@@ -31,10 +31,13 @@ do
 	echo -e "Examining following files in path: "$ROS_PATH
         find . -regex '.*\.\(cpp\|h\|cc\|hh\)' | grep -v "${grepstr}"
 	${SCRIPTS_PATH}/cpplint `find . -regex '.*\.\(cpp\|h\|cc\|hh\)' | grep -v "${grepstr}"` 2> ${WORKSPACE}/cpplint_warnings_absolute.txt
+
+        ### Do not leave a blank line after public/private/protected
+	grep -v 'Do not leave a blank line after' ${WORKSPACE}/cpplint_warnings_absolute.txt > ${WORKSPACE}/cpplint_filter1.txt
         
 	### Make all paths relative so jenkins can find them.
         echo -e "Changing all paths so jenkins can find them"
-        sed 's/\.\//src\//g' ${WORKSPACE}/cpplint_warnings_absolute.txt >> ${WORKSPACE}/cpplint.txt
+        sed 's/\.\//src\//g' ${WORKSPACE}/cpplint_filter1.txt >> ${WORKSPACE}/cpplint.txt
 
 	
 done
@@ -43,6 +46,7 @@ cp ${WORKSPACE}/cpplint.txt ${WORKSPACE}/cpplint_warnings.txt
 
 #clenup
 rm ${WORKSPACE}/cpplint_warnings_absolute.txt
+rm ${WORKSPACE}/cpplint_filter1.txt
 rm ${WORKSPACE}/cpplint.txt
 
 echo "Done!"
