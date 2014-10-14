dd8bcfe0aadffa4d0552fce2961fae92d956640a
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2014-10-13 19:22:36 -0700

    updates

--- scripts/run_cpplint.bash
+++ scripts/run_cpplint.bash
@@ -24,29 +24,23 @@ while read p; do
 done < $IGNORE_PATH
 echo
 
-for ROS_PATH in  "$@";
+for ROS_PATH in "$@";
 do
-        cd ${ROS_PATH}
+  cd ${ROS_PATH}
 
-	echo -e "Examining following files in path: "$ROS_PATH
-        find . -regex '.*\.\(cpp\|h\|cc\|hh\)' | grep -v "${grepstr}"
-	${SCRIPTS_PATH}/cpplint `find . -regex '.*\.\(cpp\|h\|cc\|hh\)' | grep -v "${grepstr}"` 2> ${WORKSPACE}/cpplint_warnings_absolute.txt
+  echo -e "Examining following files in path: "$ROS_PATH
+  find . -regex '.*\.\(cpp\|h\|cc\|hh\)' | grep -v "${grepstr}"
+  ${SCRIPTS_PATH}/cpplint --filter=-legal/copyright,-build/header_guard `find . -regex '.*\.\(cpp\|h\|cc\|hh\)' | grep -v "${grepstr}"` 2> ${WORKSPACE}/cpplint_warnings_absolute.txt
 
-        ### Do not leave a blank line after public/private/protected
-	grep -v 'Do not leave a blank line after' ${WORKSPACE}/cpplint_warnings_absolute.txt > ${WORKSPACE}/cpplint_filter1.txt
-        
-	### Make all paths relative so jenkins can find them.
-        echo -e "Changing all paths so jenkins can find them"
-        sed 's/\.\//src\//g' ${WORKSPACE}/cpplint_filter1.txt >> ${WORKSPACE}/cpplint.txt
-
-	
+  ### Make all paths relative so jenkins can find them.
+  echo -e "Changing all paths so jenkins can find them"
+  sed 's/\.\//src\//g' ${WORKSPACE}/cpplint_warnings_absolute.txt >> ${WORKSPACE}/cpplint.txt
 done
 
 cp ${WORKSPACE}/cpplint.txt ${WORKSPACE}/cpplint_warnings.txt
 
 #clenup
 rm ${WORKSPACE}/cpplint_warnings_absolute.txt
-rm ${WORKSPACE}/cpplint_filter1.txt
 rm ${WORKSPACE}/cpplint.txt
 
 echo "Done!"
--- scripts/run_pep8.bash
+++ scripts/run_pep8.bash
@@ -17,13 +17,13 @@ do
 	echo -e "Examining following files in path: "$ROS_PATH
         find . -regex '.*\.\(cpp\|h\|cc\|hh\)' | grep -v "${grepstr}"
         find . -iname "*.py"
-        find . -iname "*.py" -exec pep8 '{}' \; > ${WORKSPACE}/pep8_warnings_absolute.txt
-        
+        find . -iname "*.py" -exec pep8 --max-line-length=120 '{}' \; > ${WORKSPACE}/pep8_warnings_absolute.txt
+
 	### Make all paths relative so jenkins can find them.
         echo -e "Changing all paths so jenkins can find them"
         sed 's/\.\//src\//g' ${WORKSPACE}/pep8_warnings_absolute.txt >> ${WORKSPACE}/pep8.txt
 
-	
+
 done
 
 cp ${WORKSPACE}/pep8.txt ${WORKSPACE}/pep8_warnings.txt
