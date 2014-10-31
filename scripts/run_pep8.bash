41676591684012907a203fbcab4f465e7f557697
Jenkins <jenkins@sectoid.(none)>
2014-10-31 09:57:15 -0700

    pep8 is being applied to python executables w/o .py extension too

--- scripts/run_pep8.bash
+++ scripts/run_pep8.bash
@@ -1,34 +1,36 @@
 #!/bin/bash
+
 # Pep8
 
-# variables
+# Variables
 SCRIPTS_PATH=${JENKINS_HOME}/jenkins_config/scripts
 IGNORE_PATH=${JENKINS_HOME}/jenkins_config/cpplint_ignore
 
 echo
-echo "============================================================================"
+echo "================================================================="
 echo "Executing Pep8"
-echo "============================================================================"
+echo "================================================================="
 
 for ROS_PATH in  "$@";
 do
-        cd ${ROS_PATH}
+  cd ${ROS_PATH}
 
-	echo -e "Examining following files in path: "$ROS_PATH
-        find . -regex '.*\.\(cpp\|h\|cc\|hh\)' | grep -v "${grepstr}"
-        find . -iname "*.py"
-        find . -iname "*.py" -exec pep8 --max-line-length=120 '{}' \; > ${WORKSPACE}/pep8_warnings_absolute.txt
+  echo -e "Examining following files in path: "$ROS_PATH
+  # Apply linting to files with python extentions
+  find . -iname "*.py"
+  find . -iname "*.py" -exec pep8 --max-line-length=120 '{}' \; > ${WORKSPACE}/pep8_warnings_absolute.txt
+  # Apply linting to files with python shebang line
+  grep -rl '^#!/.*python' . | grep -v .py
+  grep -rl '^#!/.*python' . | grep -v .py | xargs pep8 --max-line-length=120 >> ${WORKSPACE}/pep8_warnings_absolute.txt
 
-	### Make all paths relative so jenkins can find them.
-        echo -e "Changing all paths so jenkins can find them"
-        sed 's/\.\//src\//g' ${WORKSPACE}/pep8_warnings_absolute.txt >> ${WORKSPACE}/pep8.txt
-
-
+  # Make all paths relative so jenkins can find them.
+  echo -e "Changing all paths so jenkins can find them"
+  sed 's/\.\//src\//g' ${WORKSPACE}/pep8_warnings_absolute.txt >> ${WORKSPACE}/pep8.txt
 done
 
 cp ${WORKSPACE}/pep8.txt ${WORKSPACE}/pep8_warnings.txt
 
-#clenup
+# Clenup
 rm ${WORKSPACE}/pep8_warnings_absolute.txt
 rm ${WORKSPACE}/pep8.txt
 
